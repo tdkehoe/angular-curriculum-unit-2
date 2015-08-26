26a1968d92840585fa726631b25a976f3c9a62e8
Thomas David Kehoe <kehoe@casafuturatech.com>
2015-08-25 21:15:12 -0600

    Pokemon progress

--- Unit-2/pokemon/javascript/controllers/PokemonController.js
+++ Unit-2/pokemon/javascript/controllers/PokemonController.js
@@ -2,30 +2,22 @@ angular.module('PokemonApp').controller('PokemonController', ["$scope", "$http",
    $scope.message = "Connected!";
 
    $http({method: 'GET', url: "http://pokeapi.co/api/v1/pokedex/1/"}).success(function(data) {
-    var aPokemon = data.pokemon[0].resource_uri;
-    var aPokemonID = data.pokemon[0].resource_uri.split('/');
+     var       pokemons = [],
+     fiveRandomPokemons = [];
 
-    var pokemons = [];
-    var fiveRandomPokemons = [];
-    var aPokemonID3 = data.pokemon[0].resource_uri.split('/')[3];
+     for (var i = 0; i < data.pokemon.length; i++) {
+       pokemons.push(Number(data.pokemon[i].resource_uri.split('/')[3]));
+     }
 
+     for (var i = 0; i < 5; i++) {
+       fiveRandomPokemons.push(pokemons[Math.floor(Math.random()*pokemons.length)]);
+     }
 
-    for (var i = 0; i < data.pokemon.length; i++) {
-      pokemons.push(data.pokemon[i].resource_uri.split('/')[3]);
-    }
+     console.log(fiveRandomPokemons);
 
-    for (var i = 0; i < 5; i++) {
-      fiveRandomPokemons.push(pokemons[Math.floor(Math.random()*pokemons.length)]);
-    }
-
-    // var pokemon1 = pokemons[Math.floor(Math.random()*pokemons.length)];
-
-
-
-
-    console.log(pokemons);
-    // console.log(pokemon1);
-    console.log(fiveRandomPokemons);
+     $http({method: 'GET', url: "http://pokeapi.co/api/v1/pokemon/" + fiveRandomPokemons[0] + "/"}).success(function(data) {
+       console.log(data);
+     });
 
    });
 
